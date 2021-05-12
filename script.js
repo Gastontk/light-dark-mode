ac14b437c8997cf870b933eca7e6cdaf035c4fa3
Gaston Kennedy <gastonkennedy@Gastons-MacBook-Air.local>
2021-05-12 13:31:39 -0700

    final commit?

--- script.js
+++ script.js
@@ -1,13 +1,67 @@
 const toggleSwitch = document.querySelector('input[type="checkbox"]');
+const nav = document.getElementById('nav')
+const toggleIcon = document.getElementById('toggle-icon')
+const image1 = document.getElementById('image1')
+const image2 = document.getElementById('image2')
+const image3 = document.getElementById('image3')
+const textBox = document.getElementById('text-box')
+
+// methods to enable dark or light mode
+//Thesse two functions can be combined into one using ternary operators
+function darkMode(){
+    nav.style.backgroundColor ='rgb(0 0 0 / 50%';
+    textBox.style.backgroundColor =  'rgb(255 255 255 /50%';
+    toggleIcon.children[0].textContent = 'Dark Mode';
+    toggleIcon.children[1].classList.replace('fa-sun', 'fa-moon');
+    imageMode('dark')
+}
+function lightMode(){
+    nav.style.backgroundColor =  'rgb(255 255 255 /50%';
+    textBox.style.backgroundColor ='rgb(0 0 0 / 50%';
+    toggleIcon.children[0].textContent = 'Light Mode';
+    toggleIcon.children[1].classList.replace('fa-moon', 'fa-sun')
+    imageMode('light')
+
+}
+
+function imageMode(color){
+    image1.src = `img/undraw_proud_coder_${color}.svg`
+    image2.src = `img/undraw_feeling_proud_${color}.svg`
+    image3.src = `img/undraw_progressive_app_m9ms_${color}.svg`
+}
 
 
 // Switch theme method
 function switchTheme(event){
     if(event.target.checked){
         document.documentElement.setAttribute('data-theme', 'dark');
+        darkMode()
+        localStorage.setItem('theme', 'dark')
     } else {
         document.documentElement.setAttribute('data-theme', 'light')
+        lightMode()
+        localStorage.setItem('theme', 'light')
+
     }
+}
+function checkLocalStorage(){
+    let whichMode = localStorage.getItem('theme');
+    console.log(whichMode);
+    if(whichMode == 'dark'){
+        toggleSwitch.setAttribute('checked', true);
+
+        document.documentElement.setAttribute('data-theme', 'dark');
+        darkMode()
+        // localStorage.setItem('theme', 'dark')
+
+    }  else{
+        document.documentElement.setAttribute('data-theme', 'light')
+        lightMode()
+        
+
+    }
+
+    
 }
 async function checkIP(){
     fetch('https://ipapi.co/json/')
@@ -50,4 +104,5 @@ function sendIpToFirebase(res){
 toggleSwitch.addEventListener('change', switchTheme);
 
 checkIP();
+checkLocalStorage()
 
